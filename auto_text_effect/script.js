cb03c5bf56189e2b7d265753d7033b870e6cd10a
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2024-06-07 21:42:39 +0900

    Added event listener to display user\'s custom message.

--- auto_text_effect/script.js
+++ auto_text_effect/script.js
@@ -1,11 +1,11 @@
 const textEl = document.querySelector('#text');
 const speedEl = document.querySelector('#speed');
-const text = 'Let\'s play guitar';
+const textInput = document.querySelector('#textInput');
+let text = textInput.placeholder;
 let idx = 1;
 let speed = 300 / speedEl.value;
 
 writeText();
-
 // Display text on screen 1 char at a time
 function writeText() {
     textEl.innerText = text.slice(0, idx);
@@ -19,4 +19,9 @@ function writeText() {
 // Adjust speed
 speedEl.addEventListener('input', (e) => {
     speed = 300 / e.target.value;
+})
+
+textInput.addEventListener('input', (e) => {
+    text = e.target.value || textInput.placeholder;
+    idx = 1;
 })
