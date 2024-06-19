ca80615f77b26e0474a410bea7d5dd2fbd88885b
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2024-06-19 14:28:39 +0900

    Randomized password characters

--- password_generator/script.js
+++ password_generator/script.js
@@ -51,9 +51,18 @@ function generatePassword(upper, lower, number, symbol, length) {
         })
     }
 
-    const finalPassword = generatedPassword.slice(0, length);
+    const finalPassword = shuffle(generatedPassword.slice(0, length));
 
     return finalPassword;
+}
+
+function shuffle(string) {
+    const array = string.split('');
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array.join('');
 }
 
 function getRandomLower() {
