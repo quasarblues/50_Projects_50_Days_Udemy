58c1d9348d5013fd71c046b84a4201fd00d6ed74
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2024-06-19 14:11:34 +0900

    Initial commit.

---
+++ password_generator/script.js
@@ -0,0 +1,74 @@
+const resultEl = document.querySelector('#result');
+const clipboardEl = document.querySelector('#clipboard');
+const lowercaseEl = document.querySelector('#lowercase');
+const uppercaseEl = document.querySelector('#uppercase');
+const numbersEl = document.querySelector('#numbers');
+const symbolsEl = document.querySelector('#symbols');
+const generateBtn = document.querySelector('#generate');
+const lengthEl = document.querySelector('#length');
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNum,
+    symbol: getRandomSymbol
+}
+
+clipboardEl.addEventListener('click', () => {
+    const password = resultEl.innerText;
+    if (!password) return;
+
+    navigator.clipboard.writeText(password).then(() => {
+        alert('Password copied to clipboard');
+    }).catch(err => {
+        console.error('Failed to copy text: ', err);
+    });
+});
+
+generateBtn.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, length);
+})
+
+function generatePassword(upper, lower, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = upper + lower + number + symbol;
+    const typesArr = [{ upper }, { lower }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+
+    if (typesCount === 0) {
+        return '';
+    }
+
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]();
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length);
+
+    return finalPassword;
+}
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNum() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.￦';
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
