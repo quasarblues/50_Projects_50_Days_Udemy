e0837e2b5659c9ce815ed44a9e02ce0598860634
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2024-07-08 20:59:33 +0900

    Completed timer functionality

--- simple_timer/script.js
+++ simple_timer/script.js
@@ -9,50 +9,29 @@ const projectContainer = document.querySelector('#project-container');
 const editTimer = document.querySelector('#edit-timer');
 const confirmBtn = document.querySelector('#confirm');
 const cancelBtn = document.querySelector('#cancel');
+const hoursInput = document.querySelector('#hours');
+const minutesInput = document.querySelector('#minutes');
+const secondsInput = document.querySelector('#seconds');
 
 // Initial Setup
-const totalSeconds = 3600;
+let totalSeconds = 0;
 let playing = false;
 let currentSeconds = totalSeconds;
 timerEl.innerText = formatTime(totalSeconds);
 
-// Run the run() function every second
 const timerInterval = setInterval(run, 1000);
 
-// Event Listeners
 playBtn.addEventListener('click', () => {
     playing = !playing;
     playBtn.classList.toggle('play');
-    playBtn.classList.toggle('bg-green-500');
+    playBtn.classList.toggle('bg-green-500'); // Toggle the color class
     const playIcon = playBtn.querySelector('i');
-    playIcon.classList.toggle('fa-play');
-    playIcon.classList.toggle('fa-pause');
+    playIcon.classList.toggle('fa-play'); // Toggle the play icon
+    playIcon.classList.toggle('fa-pause'); // Toggle the pause icon
 });
+resetBtn.addEventListener('click', resetAll);
 
-resetBtn.addEventListener('click', () => {
-    resetAll();
-})
-
-// Launch Edit Timer Screen
-projectContainer.addEventListener('click', (e) => {
-    if (e.target.matches('#reset, #play, .fas')) {
-        e.stopPropagation();
-    } else {
-        editTimer.classList.remove('hidden');
-        projectContainer.classList.remove('hover:bg-gray-800', 'hover:-translate-y-1', 'group');
-        projectContainer.classList.add('active');
-    }
-})
-
-// Close Edit Timer Screen
-cancelBtn.addEventListener('click', (e) => {
-    e.stopPropagation();
-    editTimer.classList.add('hidden');
-    projectContainer.classList.add('hover:bg-gray-800', 'hover:-translate-y-1', 'group');
-    projectContainer.classList.remove('active');
-})
-
-// Run Timer
+// Run the timer
 function run() {
     if (playing) {
         currentSeconds -= 1;
@@ -60,43 +39,82 @@ function run() {
             clearInterval(timerInterval);
             resetAll();
         }
+
         timerEl.innerText = formatTime(currentSeconds);
-        root.style.setProperty('--degrees', calcDeg())
+        root.style.setProperty('--degrees', calcDeg());
     }
 }
 
-// Calculate degrees
-function calcDeg() {
-    return `${360 - (currentSeconds / totalSeconds) * 360}deg`;
-}
-
-// Reset the timer 
-function resetAll() {
-    playing = false;
-    playBtn.classList.remove('play');
-    playBtn.classList.remove('bg-green-500');
-    const playIcon = playBtn.querySelector('i');
-    playIcon.classList.add('fa-play');
-    playIcon.classList.remove('fa-pause');
-    currentSeconds = totalSeconds;
-    timerEl.innerText = formatTime(totalSeconds);
-    root.style.setProperty('--degrees', '0deg');
-}
-
-// Format time
+// Format the time
 function formatTime(seconds) {
-    const newSeconds = seconds % 60;
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
+    const hours = Math.floor(seconds / 3600);
+    const remainingSecondsAfterHours = seconds % 3600;
+    const minutes = Math.floor(remainingSecondsAfterHours / 60);
+    const newSeconds = remainingSecondsAfterHours % 60;
 
     return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${newSeconds.toString().padStart(2, '0')}`;
 }
 
-// Timer Edit
-const hoursInput = document.querySelector('#hours');
-const minutesInput = document.querySelector('#minutes');
-const secondsInput = document.querySelector('#seconds');
+// Calculate the degrees
+function calcDeg() {
+    return `${360 - (currentSeconds / totalSeconds) * 360}deg`;
+}
 
+// Reset all the values
+function resetAll() {
+    playing = false;
+    playBtn.classList.remove('play');
+    playBtn.classList.remove('bg-green-500'); // Remove the color class
+    const playIcon = playBtn.querySelector('i');
+    playIcon.classList.remove('fa-pause'); // Remove the pause icon
+    playIcon.classList.add('fa-play'); // Add the play icon
+    currentSeconds = totalSeconds;
+    timerEl.innerText = formatTime(totalSeconds);
+    root.style.setProperty('--degrees', '0deg');
+}
+// ************ Edit Time Screen ****************
+// **********************************************
+
+// Launch Edit Timer Screen
+projectContainer.addEventListener('click', (e) => {
+    if (!playing) {
+        if (e.target.matches('#reset, #play, .fas')) {
+            e.stopPropagation();
+        } else {
+            editTimer.classList.remove('hidden');
+            projectContainer.classList.remove('hover:bg-gray-800', 'hover:-translate-y-1', 'group');
+            projectContainer.classList.add('active');
+        }
+    }
+})
+
+// Handle confirm button
+confirmBtn.addEventListener('click', (e) => {
+    const hours = +hoursInput.value;
+    const minutes = +minutesInput.value;
+    const seconds = +secondsInput.value;
+
+    totalValueinSeconds = (hours * 3600) + (minutes * 60) + seconds;
+
+    totalSeconds = totalValueinSeconds;
+    currentSeconds = totalSeconds;
+    timerEl.innerText = formatTime(totalSeconds);
+
+    closeEdit(e);
+})
+
+
+// Close Edit Timer Screen
+cancelBtn.addEventListener('click', (e) => {
+    closeEdit(e);
+})
+
+function closeEdit(e) {
+    e.stopPropagation();
+    editTimer.classList.add('hidden');
+    projectContainer.classList.add('hover:bg-gray-800', 'hover:-translate-y-1', 'group');
+    projectContainer.classList.remove('active');
+}
 function formatHours(e) {
     let value = e.target.value
     // If input length is > 2, keep last 2 digits
@@ -134,9 +152,5 @@ function formatMinSec(e) {
 }
 
 hoursInput.addEventListener('input', formatHours);
-
 minutesInput.addEventListener('input', formatMinSec);
-
 secondsInput.addEventListener('input', formatMinSec);
-
-// YOu formatted the input. Tomorrow we need to make the inputs reflect the value on the timer
